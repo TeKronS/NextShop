23ee3902f69ac3392dee94537fdf9125b81054e1
Simon Enrique Ramirez Ferrer <Asper.Tk93@gmail.com>
2026-06-15 22:44:13 +0000

    que por defecto el idioma principal sea el español

--- src/components/language/language-context.tsx
+++ src/components/language/language-context.tsx
@@ -12,7 +12,7 @@ interface LanguageContextType {
 const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
 
 export function LanguageProvider({ children }: { children: React.ReactNode }) {
-  const [language, setLanguage] = useState<Language>('en');
+  const [language, setLanguage] = useState<Language>('es');
 
   useEffect(() => {
     const savedLang = localStorage.getItem('nextshop_lang') as Language;
